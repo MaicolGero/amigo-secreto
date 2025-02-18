94dfd2286b2a156397b633e55cd5e15251a57ba4
MaicolGero <maicolguerrero94@gmail.com>
2025-02-17 23:20:55 -0500

    upload

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres =[];
+
+function agregarAmigo() {
+    let amigo = (document.getElementById('amigo').value);
+    nombres.push(amigo);
+    console.log(nombres);
+}
