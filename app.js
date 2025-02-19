236360dbaa10b24177f78fe862628c63490aec40
MaicolGero <maicolguerrero94@gmail.com>
2025-02-18 22:28:35 -0500

    upload

--- app.js
+++ app.js
@@ -3,6 +3,40 @@ let nombres =[];
 
 function agregarAmigo() {
     let amigo = (document.getElementById('amigo').value);
-    nombres.push(amigo);
+    if (amigo==="") {
+        alert("Por favor, inserte un nombre.")
+    }else{
+        nombres.push(amigo);
+    }
     console.log(nombres);
+    limpiarCaja();
+    actualizarLista();
 }
+
+function limpiarCaja() {
+    document.getElementById('amigo').value='';  
+}
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML='';
+    for (let i = 0; i < nombres.length; i++) {
+        let elementoLista = document.createElement('li');
+        elementoLista.textContent =nombres[i];
+        lista.appendChild(elementoLista);
+        
+    }
+}
+function sortearAmigo() {
+    if (nombres.length === 0) {
+        alert("no hay nombres para sortear")
+    }else{
+        let numeroMaximo = nombres.length;
+        let numeroGenerado = Math.floor(Math.random()*numeroMaximo);
+        let nombreElegido = nombres[numeroGenerado];
+        document.getElementById('resultado').innerHTML=nombreElegido;
+
+    }
+    
+}
+    
